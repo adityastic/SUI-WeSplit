35dd768b03ca09320ebe5d952a978801231e1a75
Aditya Gupta <adityaofficialgupta@gmail.com>
2019-12-18 00:27:35 +0000

    Initial Commit

---
+++ SwiftUI WeSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI WeSplit
+//
+//  Created by Aditya Gupta on 12/18/19.
+//  Copyright © 2019 Aditya Gupta. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
